340cbac6a6066ea27655e744ffbbd9796d4ec722
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2024-10-14 12:29:50 -0500

    update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // src/server.js
-const app = require('./app');
+const app = require('./src/app');
 require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 
 // Iniciar el servidor
 app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`Servidor lambda corriendo`);
 });
